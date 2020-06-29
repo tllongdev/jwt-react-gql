ea73895dbb1d16e5c5093a59ebc4522d75c2f424
Timothy Lee Long <tllongdev@gmail.com>
2020-06-29 00:23:03 -0500

    added cookie-parser and types, sending refreshtoken in Postman successfully returns accessToken 011330

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,5 @@
+import { createAccessToken } from "./auth";
+import { verify } from "jsonwebtoken";
 import "dotenv/config";
 import "reflect-metadata";
 import express from "express";
@@ -5,10 +7,37 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
+import cookieParser from "cookie-parser";
+import { User } from "./entity/User";
 
 (async () => {
   const app = express();
+  app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello"));
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    // token is valid
+    // and we can send back an access token
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
 
   await createConnection();
 
